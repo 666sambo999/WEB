fc60d1831624953cb1284a5c3e37ea713328f473
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2024-11-10 00:50:02 +0500

    Movies_done

--- Blasor/BlazorMoveApp/Move.cs
+++ Blasor/BlazorMoveApp/Move.cs
@@ -9,10 +9,11 @@ public class Movie
     [Required]
     public string? Title { get; set; }
     [DataType(DataType.Date)]
+    [Range(typeof(DateTime), "1888-10-14", "2025-12-31")]
     public DateTime? DateRelies { get; set; }
     // знак ? ставиться ссылочных типов данных
     public string? Genre { get; set; }
-
+    [Range(150, 1000)]// ограничение со стоймости 
     public decimal Price { get; set; }
   
 }
